30c95a5c111dde2cf308f9afeeb6cc9d212c93ce
Eric Quackenbush <ericmquackenbush@gmail.com>
2022-05-22 17:00:24 -0400

    Created JS object to hold information about services rendered

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!")
+const services = {
+    washCar: {
+        name: "Wash Car",
+        price: 10,
+        requested: false
+    },
+    mowLawn: {
+        name: "Mow Lawn",
+        price: 20,
+        requested: false
+    },
+    pullWeeds: {
+        name: "Pull Weeds",
+        price: 30,
+        requested: false
+    }
+}
