1e45d12c0bae0588fb0e2c7fb91b79ed9ad8a94b
Eric Quackenbush <ericmquackenbush@gmail.com>
2022-05-22 21:26:19 -0400

    Used the services object to map each key containing a service to a button in the HTML

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ const services = {
         requested: false
     }
 }
+
+const btnContainer = document.querySelector('#btn-container')
+
+Object.keys(services).forEach(e => {
+    const btn = document.createElement("button")
+    btn.innerText = services[e].name
+    btn.addEventListener("click", function() {
+        services[e].requested = !services[e].requested
+        btn.style.border = (services[e].requested) ? "solid red" : ""
+    })
+    btnContainer.appendChild(btn)
+}) 
