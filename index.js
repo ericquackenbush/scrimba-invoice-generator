e2e8da803d065ba737b766383ca01cf1fd22824d
Eric Quackenbush <ericmquackenbush@gmail.com>
2022-05-30 17:59:17 -0400

    Reset button is working

--- index.js
+++ index.js
@@ -19,13 +19,16 @@ const services = {
 const btnContainer = document.querySelector('#btn-container')
 const requestedServices = document.querySelector('#requested-services')
 const totalAmount = document.querySelector("#total-amount")
+const resetBtn = document.querySelector("#reset-button")
 
 // display buttons to add/remove a service
 Object.keys(services).forEach(e => {
     const btn = document.createElement("button")
     btn.innerText = services[e].name
     btn.addEventListener("click", function() {
+        // replace with a toggle
         services[e].requested = !services[e].requested
+        // add to updateRequestedServices() function
         btn.style.border = (services[e].requested) ? "solid red" : ""
         // call function to update display
         updateRequestedServices()
@@ -53,3 +56,20 @@ function updateTotalAmount() {
     })
     totalAmount.innerHTML = `Total: \$${currentTotal}` 
 }
+
+resetBtn.addEventListener("click", function() {
+    // remove red borders around each button
+    btnContainer.childNodes.forEach(e => {
+        if (e.nodeName === 'BUTTON') {
+            e.style.border = ""
+        }
+    })
+    // set requested to false for each service
+    Object.keys(services).forEach(e => {
+        services[e].requested = false
+    }) 
+    // call function to update display
+    updateRequestedServices()
+    // call function to update total
+    updateTotalAmount()
+})
