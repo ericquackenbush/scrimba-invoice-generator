5d403050c51b17720733ea4256eac1d820c8ae74
Eric Quackenbush <ericmquackenbush@gmail.com>
2022-05-30 00:41:20 -0400

    Finished displaying data from array

--- index.js
+++ index.js
@@ -17,13 +17,26 @@ const services = {
 }
 
 const btnContainer = document.querySelector('#btn-container')
+const requestedServices = document.querySelector('#requested-services')
 
+// display buttons to add/remove a service
 Object.keys(services).forEach(e => {
     const btn = document.createElement("button")
     btn.innerText = services[e].name
     btn.addEventListener("click", function() {
         services[e].requested = !services[e].requested
         btn.style.border = (services[e].requested) ? "solid red" : ""
+        // call function to update display
+        updateRequestedServices()
     })
     btnContainer.appendChild(btn)
 }) 
+
+function updateRequestedServices() {
+    requestedServices.innerHTML = ''
+    Object.keys(services).forEach(e => {
+        if (services[e].requested === true) {
+            requestedServices.innerHTML += `<p>${services[e].name} \$${services[e].price}</p>`
+        }
+    }) 
+}
